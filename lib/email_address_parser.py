1636a8208e657d2734d08220c73da8296d15756e
Maria Jenkins <macjenkin@gmail.com>
2023-10-04 07:10:39 -0500

    complete lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+
+        email_format = re.compile(r'[A-Za-z][a-zA-Z0-9_\.]+\@\w+\.\w+')
+        valid_emails = re.findall(email_format, self.email_addresses)
+
+        return sorted(list(set(valid_emails)))
